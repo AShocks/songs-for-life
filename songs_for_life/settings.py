5f919284ce16f9e89af32056ab03b3b319217ffb
AShocks <oshaughnessy.adrian@gmail.com>
2022-12-06 15:06:11 +0000

    Change text layout and colour in footer in base.html

--- songs_for_life/settings.py
+++ songs_for_life/settings.py
@@ -59,6 +59,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-info',
         messages.INFO: 'alert-info',
