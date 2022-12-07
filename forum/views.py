4e68200c03927534a81ad0c8c5db9acae7b33fd8
AShocks <oshaughnessy.adrian@gmail.com>
2022-12-07 18:04:17 +0000

    Fix indentation errors on views.py

--- forum/views.py
+++ forum/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View 
+from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
 
 class PostList(generic.ListView):
-    """Displays The Post List page"""
+    """ Displays the PostList views """
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
 
 
 class PostDetail(View):
-    """Displays Post Detail page"""
+
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
@@ -34,7 +34,7 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
-        
+
     def post(self, request, slug, *args, **kwargs):
 
         queryset = Post.objects.filter(status=1)
@@ -45,7 +45,6 @@ class PostDetail(View):
             liked = True
 
         comment_form = CommentForm(data=request.POST)
-        
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
@@ -69,7 +68,7 @@ class PostDetail(View):
 
 
 class PostLike(View):
-    
+
     def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
