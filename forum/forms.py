f38b27d5538f2d3b065f2e0d82b0ae260614a188
AShocks <oshaughnessy.adrian@gmail.com>
2023-02-09 22:56:21 +0000

    Add like buttons and view

--- forum/forms.py
+++ forum/forms.py
@@ -3,6 +3,9 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    """
+    Form for adding comments to blog post
+    """
     class Meta:
         model = Comment
         fields = ('body',)
