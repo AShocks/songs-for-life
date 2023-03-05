42eeaa1c47fc2117db47ac2a84f25f455fd1626f
AShocks <oshaughnessy.adrian@gmail.com>
2023-03-05 02:08:34 +0000

    Change hero image on landing page

--- forum/models.py
+++ forum/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -41,6 +42,14 @@ class Post(models.Model):
         total number of likes on a post
         """
         return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
