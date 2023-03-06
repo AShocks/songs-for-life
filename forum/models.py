256faeabe87a073299936d9727ce110dc33a4f3f
AShocks <oshaughnessy.adrian@gmail.com>
2023-03-06 03:57:33 +0000

    Add layout to confirm delete post html file

--- forum/models.py
+++ forum/models.py
@@ -39,7 +39,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("article_detail", kwargs={"slug": self.slug})
+        return reverse("post_detail", kwargs={"slug": self.slug})
 
     def save(self, *args, **kwargs):
         if not self.slug:
